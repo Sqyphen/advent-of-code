f9500cf5a944602e33fca8c269f17bd015afef43
Sqyphen <tobias.fieldhouse@gmail.com>
2020-12-03 07:48:33 +0000

    let the code formatter do it's thing

--- 2020/1/2.js
+++ 2020/1/2.js
@@ -1,22 +1,26 @@
 // Day 1 : Part 2
 
-'use_strict';
+"use_strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
-const fileData = fs.readFileSync(__dirname + '/input.txt').toString().split('\n').map(Number);
+const fileData = fs
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .split("\n")
+  .map(Number);
 
 let result;
 
-fileData.forEach(first => {
-	fileData.forEach(second => {
-		fileData.forEach(third => {
-			const combine = first + second + third;
-			if(combine === 2020){
-				result = first * second * third;
-			}
-		});
-	});
+fileData.forEach((first) => {
+  fileData.forEach((second) => {
+    fileData.forEach((third) => {
+      const combine = first + second + third;
+      if (combine === 2020) {
+        result = first * second * third;
+      }
+    });
+  });
 });
 
 console.log(result);
--- 2020/2/2.js
+++ 2020/2/2.js
@@ -1,23 +1,26 @@
 // Day 2 : Part 2
 
-'use_strict';
+"use_strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
-const puzzleData = fs.readFileSync(__dirname + '/input.txt').toString().split('\n');
+const puzzleData = fs
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .split("\n");
 
 let validPasswordCount = 0;
 
-puzzleData.forEach(puzzle => {
-	const result = puzzle.match(/(\d?\d)-(\d?\d) (.): (.*)/);
-	const [ match, first, second, char, password ] = result;
-	const passwordArray = password.split('');
-	const firstChar = passwordArray[first - 1];
-	const secondChar = passwordArray[second - 1];
+puzzleData.forEach((puzzle) => {
+  const result = puzzle.match(/(\d?\d)-(\d?\d) (.): (.*)/);
+  const [match, first, second, char, password] = result;
+  const passwordArray = password.split("");
+  const firstChar = passwordArray[first - 1];
+  const secondChar = passwordArray[second - 1];
 
-	if((firstChar === char || secondChar === char) && (firstChar !== secondChar)){
-		validPasswordCount++;
-	}
+  if ((firstChar === char || secondChar === char) && firstChar !== secondChar) {
+    validPasswordCount++;
+  }
 });
 
 console.log(validPasswordCount);
